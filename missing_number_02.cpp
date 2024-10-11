b7f7557da625c00fdd10497d85b68d0a0310ae21
rajansingh126 <rajansingh895876@gmail.com>
2024-10-11 22:28:24 +0530

    using xor

---
+++ missing_number_02.cpp
@@ -0,0 +1,29 @@
+//buteforce
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        int Tsum = (n*(n+1))/2;
+        return  Tsum - accumulate(nums.begin(),nums.end(),0);
+        
+    }
+}; 
+//optimal using xor
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans =0;
+        for(int i =1;i<=n;i++){
+            ans = ans ^ i;
+        }
+        for(int i =0;i<nums.size();i++){
+            ans= ans^nums[i];
+        }
+        return ans;
+    }
+};
+
+
+
